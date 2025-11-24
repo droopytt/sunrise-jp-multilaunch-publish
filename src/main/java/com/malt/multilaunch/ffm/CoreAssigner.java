817ce2cfdb629416c03ae96f210e91dbfb83799a
droopytt <41344599+droopytt@users.noreply.github.com>
2025-11-24 23:49:08 +0000

    Use priority queue to assign cores

--- src/main/java/com/malt/multilaunch/ffm/CoreAssigner.java
+++ src/main/java/com/malt/multilaunch/ffm/CoreAssigner.java
@@ -1,11 +1,16 @@
 package com.malt.multilaunch.ffm;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CoreAssigner {
     private static final Logger LOG = LoggerFactory.getLogger(CoreAssigner.class);
-    private final long[] assignedCores;
+    private final PriorityQueue<Integer> freeCores;
+    private final long[] coreToPid;
+    private final Map<Long, Integer> pidToCore = new HashMap<>();
     private final int totalCores;
     private int spilloverCoreCount;
     private final int startingCore;
@@ -15,11 +20,22 @@ public class CoreAssigner {
             throw new IllegalArgumentException(
                     "Cannot have a starting core that is greater than the total number of cores");
         }
-        LOG.debug("Creating core assigner with {} cores and starting core {}", totalCores, startingCore);
-        this.assignedCores = new long[totalCores];
-        this.totalCores = totalCores;
         this.startingCore = startingCore;
+        LOG.debug("Creating core assigner with {} cores and starting core {}", totalCores, startingCore);
+        this.totalCores = totalCores;
         this.spilloverCoreCount = startingCore;
+
+        this.coreToPid = new long[totalCores];
+
+        this.freeCores = new PriorityQueue<>((a, b) -> Integer.compare(rot(a), rot(b)));
+
+        for (int i = 0; i < totalCores; i++) {
+            freeCores.add(i);
+        }
+    }
+
+    private int rot(int core) {
+        return (core - startingCore + totalCores) % totalCores;
     }
 
     public static CoreAssigner create(int totalCores) {
@@ -41,36 +57,27 @@ public class CoreAssigner {
     }
 
     long[] assignedCores() {
-        return assignedCores.clone();
+        return this.coreToPid;
     }
 
     public synchronized int getNextAvailableCore(long pid) {
-        for (int i = startingCore; i < assignedCores.length; i++) {
-            if (assignedCores[i] == 0) {
-                assignedCores[i] = pid;
-                return i;
-            }
+        Integer core = freeCores.poll();
+        if (core != null) {
+            pidToCore.put(pid, core);
+            coreToPid[core] = pid;
+            return core;
         }
 
-        for (int i = 0; i < startingCore; i++) {
-            if (assignedCores[i] == 0) {
-                assignedCores[i] = pid;
-                return i;
-            }
-        }
-
-        var nextCore = spilloverCoreCount++;
+        int next = spilloverCoreCount++;
         spilloverCoreCount %= totalCores;
-        LOG.warn("There were no cores free to assign from, using spillover core {}", nextCore);
-        return nextCore;
+        return next;
     }
 
     public synchronized void removeAssignedCore(long pid) {
-        for (int i = 0; i < assignedCores.length; i++) {
-            if (pid == assignedCores[i]) {
-                assignedCores[i] = 0;
-                return;
-            }
+        Integer core = pidToCore.remove(pid);
+        if (core != null) {
+            coreToPid[core] = 0;
+            freeCores.add(core);
         }
     }
 }
